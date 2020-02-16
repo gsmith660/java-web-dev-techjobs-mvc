754109993d45d32779a7ffd3036d7070c056f543
gsmith660 <gsmith660@gmail.com>
2020-02-16 17:03:18 -0600

    Super Bonus Mission done

---
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/TechJobsController.java
@@ -0,0 +1,33 @@
+package org.launchcode.javawebdevtechjobsmvc.controllers;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.HashMap;
+
+public class TechJobsController {
+
+    static HashMap<String, String> actionChoices = new HashMap<>();
+    static HashMap<String, String> columnChoices = new HashMap<>();
+
+    public TechJobsController() {
+        actionChoices.put("list", "List");
+        actionChoices.put("search", "Search");
+
+        columnChoices.put("all", "All");
+        columnChoices.put("employer", "Employer");
+        columnChoices.put("location", "Location");
+        columnChoices.put("positionType", "Position Type");
+        columnChoices.put("coreCompetency", "Skill");
+    }
+
+    @ModelAttribute("actions")
+    static HashMap<String, String> getActionChoices() {
+        return actionChoices;
+    }
+
+    @ModelAttribute("columnChoices")
+    static HashMap<String, String> getColumnChoices() {
+        return columnChoices;
+    }
+
+}
